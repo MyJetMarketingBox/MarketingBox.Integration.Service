00dae3e4c347588419b156a6cf79818c75ca7f9d
Yuriy Plyaskin <yuriy.p@smplt.net>
2021-11-01 15:46:40 +0300

    Fixed

--- test/MarketingBox.Integration.Service.Tests/TestExample.cs
+++ test/MarketingBox.Integration.Service.Tests/TestExample.cs
@@ -4,16 +4,18 @@ using NUnit.Framework;
 
 namespace MarketingBox.Integration.Service.Tests
 {
-    public class LanguageTest
+    public class TestExample
     {
-        [TestCase("99.99.99.22", "EN")]
-        [TestCase("ES", "CA")]
-        [TestCase("UA", "UK")]
-        [TestCase("PL", "PL")]
-        public void Test(string country, string language)
+        [SetUp]
+        public void Setup()
         {
-            var search = LanguageUtils.GetIso2LanguageFromCountryFirstOrDefault(country);
-            Assert.AreEqual(language, search);
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Console.WriteLine("Debug output");
+            Assert.Pass();
         }
     }
 }
