8b6a110cbfff2fc0ba3face92452c52b16e161eb
Yuriy Plyaskin <yuriy.p@smplt.net>
2021-12-07 18:59:46 +0300

    Update

--- src/MarketingBox.Integration.Service/Services/DepositorsBackgroundService.cs
+++ src/MarketingBox.Integration.Service/Services/DepositorsBackgroundService.cs
@@ -19,20 +19,18 @@ namespace MarketingBox.Integration.Service.Services
         private readonly IDepositUpdateStorage _depositUpdateStorage;
         private readonly IDepositService _depositRegistrationService;
         private readonly BridgeStorage _bridgeStorage;
-        private readonly BridgeServiceWrapper _bridgeServiceWrapper;
+
         public DepositorsBackgroundService(
             ILogger<DepositorsBackgroundService> logger,
             IDepositUpdateStorage depositUpdateStorage,
             IDepositService depositRegistrationService,
-            BridgeStorage bridgeStorage, 
-            BridgeServiceWrapper bridgeServiceWrapper)
+            BridgeStorage bridgeStorage)
         {
             _logger = logger;
             _depositUpdateStorage = depositUpdateStorage;
             _depositRegistrationService = depositRegistrationService;
             _operationsTimer = new MyTaskTimer(nameof(DepositorsBackgroundService), TimeSpan.FromSeconds(TimerSpan30Sec), logger, Process);
             _bridgeStorage = bridgeStorage;
-            _bridgeServiceWrapper = bridgeServiceWrapper;
         }
         public void Start()
         {
@@ -48,55 +46,55 @@ namespace MarketingBox.Integration.Service.Services
         {
             try
             {
-                //var bridges = _bridgeStorage.GetAll();
-                //foreach (var bridge in bridges)
-                //{
-
-                //    var dtStartFrom = DateTime.Parse("2021-01-01");
-                //    var dtCurrent = dtStartFrom;
-                //    var request = new ReportingRequest()
-                //    {
-                //        DateFrom = dtCurrent,
-                //        DateTo = dtCurrent.AddMonths(1),
-                //        PageIndex = 1,
-                //        PageCount = 100,
-                //    };
-                //    do
-                //    {
-                //        int count = 0;
-                //        do
-                //        {
-                //            var customerInfo = 
-                //                await _bridgeServiceWrapper.TryGetService("Monfex").GetDepositorsPerPeriodAsync(request);
-
-
-                //            var depositors = await bridge.Value.GetDepositorsPerPeriodAsync(request);
-                //            count = depositors.Items.Count;
-                //            request.PageIndex++;
-
-                //        } while (count == request.PageCount);
-                //        request.DateFrom.AddMonths(1);
-                //        request.DateTo.AddMonths(1);
-                //        request.PageIndex = 1;
-                //    } while (request.DateTo > DateTime.UtcNow);
-                //}
-
-                var messages = _depositUpdateStorage.GetAll();
-                foreach (var operation in messages)
+                var bridges = _bridgeStorage.GetAll();
+                foreach (var bridge in bridges)
                 {
-                    var message = operation.Value;
-                    var storeResponse = await _depositRegistrationService.RegisterDepositAsync(
-                        new MarketingBox.Registration.Service.Grpc.Models.Deposits.Contracts.DepositCreateRequest()
+
+                    var dtStartFrom = DateTime.Parse("2021-01-01");
+                    var dtCurrent = dtStartFrom;
+                    var request = new ReportingRequest()
+                    {
+                        DateFrom = dtCurrent,
+                        DateTo = dtCurrent.AddMonths(1),
+                        PageIndex = 1,
+                        PageCount = 100,
+                    };
+                    do
+                    {
+                        int count = 0;
+                        do
                         {
-                            CustomerId = message.CustomerId,
-                            Email = message.Email,
-                            BrandName = message.IntegrationName,
-                            BrandId = message.IntegrationId,
-                            TenantId = message.TenantId,
-                            CreatedAt = DateTime.UtcNow,
-                        });
-                    _depositUpdateStorage.Remove(operation.Key);
+                            var depositors = await bridge.Value.GetDepositorsPerPeriodAsync(request);
+                            if (depositors.Items == null)
+                            {
+                                break;
+                            }
+                            count = depositors.Items.Count;
+                            request.PageIndex++;
+
+                        } while (count == request.PageCount);
+                        request.DateFrom = request.DateFrom.AddMonths(1);
+                        request.DateTo = request.DateTo.AddMonths(1);
+                        request.PageIndex = 1;
+                    } while (request.DateTo <= DateTime.UtcNow);
                 }
+
+                //var messages = _depositUpdateStorage.GetAll();
+                //foreach (var operation in messages)
+                //{
+                //    var message = operation.Value;
+                //    var storeResponse = await _depositRegistrationService.RegisterDepositAsync(
+                //        new MarketingBox.Registration.Service.Grpc.Models.Deposits.Contracts.DepositCreateRequest()
+                //        {
+                //            CustomerId = message.CustomerId,
+                //            Email = message.Email,
+                //            BrandName = message.IntegrationName,
+                //            BrandId = message.IntegrationId,
+                //            TenantId = message.TenantId,
+                //            CreatedAt = DateTime.UtcNow,
+                //        });
+                //    _depositUpdateStorage.Remove(operation.Key);
+                //}
             }
             catch (Exception e)
             {
